c8f53e1fd6c21737583fefb0f467de94bcfc6212
Ludwig Ivan <ivansierras11@gmail.com>
2024-09-26 23:05:55 -0600

    Servidor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Ludwig-Ivan.github.io',
+    base: 'Personal_Card',
+});

691370b40c305898eaa3dba69dc9d3a551ed371e
Ludwig Ivan <ivansierras11@gmail.com>
2024-09-29 13:10:05 -0600

    Fix Links Index

--- src/js/recursos_extra.js
+++ src/js/recursos_extra.js
@@ -4,11 +4,11 @@ const play_img = url + "icons/play.png";
 const pause_img = url + "icons/pause.png";
 const forward_img = url + 'icons/fast-forward.png';
 const backward_img = url + 'icons/fast-backward.png';
-const creatividad_img = url + 'img/creatividad.png';
-const flor_img = url + 'img/Flor.png';
-const cupon_sec_img = url + 'img/Cupon.webp';
-const puntos_img = url + 'img/puntos.jpeg';
-const mas_img = url + 'img/Mas.png';
+const creatividad_img = 'img/creatividad.png';
+const flor_img = 'img/Flor.png';
+const cupon_sec_img = 'img/Cupon.webp';
+const puntos_img = 'img/puntos.jpeg';
+const mas_img = 'img/Mas.png';
 
 export default {
     play_img, pause_img, forward_img,
